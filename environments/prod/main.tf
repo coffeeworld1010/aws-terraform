231e1ae7896bd7f36468b78f4f5e66ee5c740df4
coffeeworld1010 <coffeeworld@outlook.com>
2026-08-19 22:29:52 +0900

    aaa

--- environments/prod/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = var.aws_region
-}
-
-module "vpc" {
-    source = "../../modules/vpc"
-    vpc_name = var.vpc_name
-    vpc_cidr = var.vpc_cidr
-}
--- environments/prod/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-vpc_name = "my-vpc"
-vpc_cidr = "10.0.0.0/16"
-
-aws_region = "ap_northeast_2"
