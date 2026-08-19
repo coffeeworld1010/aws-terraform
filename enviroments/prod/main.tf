4a04e9bb5dd23ef99eaf861a7df851837c4ca22d
coffeeworld1010 <coffeeworld@outlook.com>
2026-08-19 22:28:34 +0900

    fix

--- enviroments/prod/main.tf
+++ enviroments/prod/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_vpc" "this" {
     cidr_block = var.vpc_cidr
     enable_dns_support = true
-    enable_dns_hostnames = ture
+    enable_dns_hostnames = true
 
     tags = {
         Name = var.vpc_name
