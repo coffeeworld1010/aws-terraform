3b90bd4fc7c392e6a40f25c9fbfa78c47afe95c7
coffeeworld1010 <coffeeworld@outlook.com>
2026-08-19 22:24:41 +0900

    Update main.tf

--- enviroments/prod/main.tf
+++ enviroments/prod/main.tf
@@ -1,9 +1,35 @@
+terraform {
+    required_version = ">= 1.6.0"
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+}
 provider "aws" {
     region = var.aws_region
 }
 
-module "vpc" {
-    source = "../../modules/vpc"
-    vpc_name = var.vpc_name
-    vpc_cidr = var.vpc_cidr
+resource "aws_vpc" "this" {
+    cidr_block = var.vpc_cidr
+    enable_dns_support = true
+    enable_dns_hostnames = ture
+
+    tags = {
+        Name = var.vpc_name
+    }
+}
+
+resource "aws_subnet" "this" {
+    for_earch = var.subnets
+
+    vpc_id = aws_vpc.this.id
+    cidr_block = each.value.cidr
+    availability_zone = each.value.az
+
+    tags = {
+        Name = each.key
+    }
 }
