1756d6f89c4bfbdb1bd39e43f1d5d90083c3260f
coffeeworld1010 <coffeeworld@outlook.com>
2026-08-19 22:24:58 +0900

    Update outptus.tf

--- modules/vpc/outptus.tf
+++ modules/vpc/outptus.tf
@@ -5,3 +5,14 @@ output "vpd_id" {
 output "vpc_cidr" {
     value = aws_vpc.my_vpc.cidr_block
 }
+
+output "subnets" {
+    value = {
+        for anme, subnet in aws_subnet.this :
+        name => {
+            id = subnet.id
+            cidr = subnet.cidr_block
+            az = subnet.availability_zone
+        }
+    }
+}
