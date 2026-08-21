d5ccc753160578915e71b86baf3625a4c5f3f9dd
coffeeworld1010 <coffeeworld@outlook.com>
2026-08-21 13:28:49 +0900

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -2,10 +2,10 @@ resource "aws_subnet" "this" {
     for_each = var.subnets
     
     vpc_id = var.vpc_id
-    sub_cird = each.value.sub_cidr
+    sub_cird = each.value.sub_cird
     availability_zone = each.value.az
 
-    tags = {
+    tags = {`
         Name = each.value.sub_name
         Enviroment = var.enviroments
         Tier = each.value.tier
